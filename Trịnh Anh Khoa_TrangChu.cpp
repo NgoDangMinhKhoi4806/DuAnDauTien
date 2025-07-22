e2c8b64419b5d57e4d6462d5e19a6bf362a454dc
NgoDangMinhKhoi4806 <minhkhoingodang@gmail.com>
2025-07-22 10:24:14 +0700

    Create Trịnh Anh Khoa_TrangChu.cpp

---
+++ Trịnh Anh Khoa_TrangChu.cpp
@@ -0,0 +1,22 @@
+Admin - Quản trị hệ thống
+Công việc:
+
+Trang dashboard:
+
+Quản lý người dùng (Student, Tutor…)
+
+Thống kê số lượt thuê gia sư, dịch vụ
+
+Tạo Moderator, phân quyền người dùng
+
+Cấu hình hệ thống
+
+Công nghệ gợi ý:
+
+AdminLTE, React Admin, hoặc đơn giản với Bootstrap
+
+Ghi chú:
+
+Có thể dùng template sẵn
+
+Đảm bảo dễ quan sát số liệu
