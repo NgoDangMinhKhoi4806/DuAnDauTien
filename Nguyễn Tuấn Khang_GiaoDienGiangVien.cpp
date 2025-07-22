25793009beefbc6baf77694c30b5b065bd1bcb46
NgoDangMinhKhoi4806 <minhkhoingodang@gmail.com>
2025-07-22 10:22:42 +0700

    Create Nguyễn Tuấn Khang_GiaoDienGiangVien.cpp

---
+++ Nguyễn Tuấn Khang_GiaoDienGiangVien.cpp
@@ -0,0 +1,26 @@
+Xử lý người dùng và phân quyền (Backend Dev - Auth)
+Công việc:
+
+Đăng nhập/đăng ký
+
+Phân quyền Guest, Student, Tutor, Moderator, Admin
+
+API xử lý:
+
+Thuê gia sư
+
+Gửi đánh giá
+
+Chat (nếu có)
+
+Công nghệ gợi ý:
+
+NodeJS + Express hoặc Laravel/Django
+
+JWT Auth hoặc session
+
+Ghi chú:
+
+Phải bảo mật dữ liệu người dùng
+
+Viết API RESTful
