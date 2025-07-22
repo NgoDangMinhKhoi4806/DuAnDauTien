26741e3a8f6a152a208c01435a3a2ae100f38858
NgoDangMinhKhoi4806 <minhkhoingodang@gmail.com>
2025-07-22 10:23:36 +0700

    Create Ngô Đặng Minh Khôi_XuLyThongTin.cpp

---
+++ Ngô Đặng Minh Khôi_XuLyThongTin.cpp
@@ -0,0 +1,12 @@
+Moderator & Quản trị nội dung
+Nhiệm vụ cụ thể:
+
+Thiết kế chức năng kiểm duyệt:
+
+Báo cáo vi phạm từ học viên
+
+Phê duyệt nội dung video từ Tutor
+
+Quản lý phản hồi, khiếu nại
+
+Xây dựng giao diện quản lý đơn giản cho Moderator.
