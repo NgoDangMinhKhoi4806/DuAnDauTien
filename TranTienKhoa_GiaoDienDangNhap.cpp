e227af810c39ed2fb660c4df0187b0ca0b635f63
NgoDangMinhKhoi4806 <minhkhoingodang@gmail.com>
2025-07-22 10:20:16 +0700

    Update TranTienKhoa_GiaoDienDangNhap.cpp

--- TranTienKhoa_GiaoDienDangNhap.cpp
+++ TranTienKhoa_GiaoDienDangNhap.cpp
@@ -1,1 +1,26 @@
+Giao diện người dùng (Frontend Dev)
+Công việc:
 
+Thiết kế giao diện:
+
+Trang chủ (Guest)
+
+Đăng nhập/Đăng ký
+
+Trang tìm gia sư (cho Student)
+
+Trang thuê gia sư, chat, đánh giá
+
+Trang đăng dịch vụ, upload video (Tutor)
+
+Công nghệ gợi ý:
+
+HTML/CSS/JS, Bootstrap hoặc ReactJS
+
+Axios gọi API
+
+Ghi chú:
+
+Đảm bảo responsive trên điện thoại và PC
+
+Giao diện đẹp, dễ dùng
